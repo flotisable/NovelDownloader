081fb1194e7549a8aafee6a6150562bf107ead1e
flotisable <s09930698@gmail.com>
2020-03-14 15:00:53 +0800

    update documentation of novelDownloader.pl

--- novelDownloader.pl
+++ novelDownloader.pl
@@ -14,11 +14,11 @@ use NovelDownloader::ProcessorFactory;
 # documents
 =head1 NAME
 
-B<wenku8Download.pl> - download novel from www.wenku8.net
+novelDownloader.pl - download novel from website
 
 =head1 SYNOPSIS
 
-B<wenku8Download.pl> [options] <url>
+B<novelDownloader.pl> [options] <url>
 
 =head1 OPTIONS
 
@@ -42,11 +42,66 @@ The supported format are shown below:
 
 =back
 
+=item B<-h>
+
+print simplest help message.
+
+=item B<--help>
+
+print detailed help message.
+
 =back
 
-=item B<-h>, B<--help>
+=head1 DESCRIPTION
 
-print help message.
+This program is written to download the novel from the website,
+currently only support the www.wenku8.com.
+
+=head1 EXAMPLES
+
+  novelDownloader.pl -o output.epub -f epub 'https://www.wenku8.net/modules/article/reader.php?aid=112'
+
+This will download the novel whose index is at 'https://www.wenku8.net/modules/article/reader.php?aid=112',
+and the output file will be named B<output.epub> with format B<epub>.
+
+=head1 FILES
+
+=over
+
+=item F<processors.txt>
+
+The configuration file of perl modules to be used to download and export the novel.
+Each line contains 3 fields delimited by ',', ex.
+
+  <pattern>,<downloader>,<exporter>
+
+The meaning of each field is described below:
+
+=over
+
+=item B<pattern>
+
+The regular expression used to match the website url,
+if the url is matched,
+the corresponding B<downloader> and B<exporter> will be used to download and export the novel.
+
+=item B<downloader>
+
+The perl module used to download the novel.
+It should satisfy the interface defined by B<NovelDownloader::Downloader> role.
+
+=item B<exporter>
+
+The perl module used to export the novel.
+It should satisfy the interface defined by B<NovelDownloader::Exporter> role.
+
+=back
+
+=back
+
+=head1 AUTHOR
+
+Flotisable <s09930698@gmail.com>
 
 =cut
 # end documents
